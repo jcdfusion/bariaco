d779ec66eee8bc8ff56798e2a6695c0dead421db
jcdfusion <jaime.cortes@upr.edu>
2022-11-20 22:28:21 -0400

    Changes regarding authentication, login and register cubits

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:bariaco/src/features/home/screens/home.dart';
 import 'package:bariaco/utils/mercado_theme.dart';
 import 'package:bariaco/utils/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:appwrite/appwrite.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-Client client = Client()
-    .setEndpoint('http://10.0.2.2/v1') // Your Appwrite Endpoint
-    .setProject('mercadopr') // Your project ID
-    .setSelfSigned(status: true); // For self s
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -1,4 +1,6 @@
+import 'package:bariaco/src/api/authentication_repository.dart';
 import 'package:bariaco/src/api/user_repository.dart';
+import 'package:bariaco/src/features/session/cubits/login_cubit.dart';
 import 'package:bariaco/src/features/session/cubits/register_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,7 +9,11 @@ import '../src/features/session/cubits/authentication_cubit.dart';
 
 List<BlocProvider> providersList = [
   BlocProvider<AuthenticationCubit>(
-      create: (BuildContext authenticationContext) => AuthenticationCubit()),
+      create: (BuildContext authenticationContext) =>
+          AuthenticationCubit(AuthenticationRepository())),
   BlocProvider<RegisterCubit>(
-      create: (BuildContext registerContext) => RegisterCubit(UserRepository()))
+      create: (BuildContext registerContext) =>
+          RegisterCubit(UserRepository())),
+  BlocProvider<LoginCubit>(
+      create: (BuildContext loginContext) => LoginCubit(UserRepository()))
 ];
